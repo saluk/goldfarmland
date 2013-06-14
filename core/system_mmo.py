83554fbf1c2ababa2b919b7c6b023eae669f2f6d
Patrick <saluk64007@gmail.com>
2013-06-13 23:26:17 -0700

    Show encounter on the mmo screen

--- core/system_mmo.py
+++ core/system_mmo.py
@@ -7,12 +7,28 @@ from .ui import Button,TextButton
 from .agents import Agent
 
 class Char(Agent):
-    def __init__(self):
+    def __init__(self,index=[[0,0],[8,8]]):
         super().__init__("art/sprites/char.png")
+        self.index = index
     def load(self):
         super().load()
-        self.subsurface([[0,0],[8,8]])
+        self.subsurface(self.index)
         self.surface = pygame.transform.scale(self.surface,[16,16])
+        
+class Encounter(Agent):
+    def __init__(self,chars):
+        super().__init__()
+        self.chars = []
+        x=0;y=0
+        for c in chars:
+            cspr = Char(c)
+            cspr.pos = [x,y]
+            x+=9
+            y+=2
+            self.chars.append(cspr)
+    def draw(self,engine,offset):
+        offset= [offset[0]+self.pos[0],offset[1]+self.pos[1]]
+        [x.draw(engine,offset) for x in self.chars]
 
 class MMOWorld(ClickWorld):
     def __init__(self,manager,engine,game):
@@ -23,3 +39,6 @@ class MMOWorld(ClickWorld):
         self.pc = Char()
         self.pc.pos = [5*32,5*32]
         self.add(self.pc)
+        self.add_encounter(Encounter([ [[56,56],[8,8]] , [[56,56],[8,8]]  ,  [[56,56],[8,8]] ]))
+    def add_encounter(self,encounter):
+        self.add(encounter)
